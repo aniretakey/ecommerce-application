72802cb59fa941d11414149bbc4e69d61ce9cb46
gorobchenko-k <gorobchenko.es@mail.ru>
2023-08-02 13:15:54 +0300

    chore: add tailwindcss

--- ecommerce-application/webpack.config.js
+++ ecommerce-application/webpack.config.js
@@ -27,7 +27,7 @@ const config = {
       },
       {
         test: /\.css$/i,
-        use: [stylesHandler, 'css-loader'],
+        use: [stylesHandler, 'css-loader', 'postcss-loader'],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|jpeg|gif)$/i,
