4b23072f9b5be38b4cab24c5642f868ce69b015a
aniretakey <EMKornishina@gmail.com>
2023-08-08 12:27:05 +0300

    feat: update routing function

--- src/router/router.ts
+++ src/router/router.ts
@@ -6,49 +6,50 @@ import Login from '../components/pages/login';
 import About from '../components/pages/about';
 import BaseComponent from '../utils/baseComponent';
 
+const pagesList = ['main-page', 'catalog-page', 'about-page', 'registration-page', 'login-page'];
+
 function renderNewPage(mainHTML: BaseComponent, newPage: HTMLElement): void {
   mainHTML.clearInnerHTML();
   mainHTML.getNode().append(newPage);
 }
 
 export default function routing(mainHTML: BaseComponent): void {
-  const router = new Navigo('/');
-
-  router.on('/main-page', function () {
+  const router = new Navigo('/', { hash: true, strategy: 'ALL' });
+  router.on('', () => {
     const MainPage = new Main().render();
     renderNewPage(mainHTML, MainPage);
   });
-
-  router.on('/catalog-page', function () {
+  router.on('/catalog-page', () => {
     const CatalogPage = new Catalog().render();
     renderNewPage(mainHTML, CatalogPage);
   });
-
-  router.on('/registration-page', function () {
+  router.on('/registration-page', () => {
     const RegistrationPage = new Registration().render();
     renderNewPage(mainHTML, RegistrationPage);
   });
-
-  router.on('/login-page', function () {
+  router.on('/login-page', () => {
     const LoginPage = new Login().render();
     renderNewPage(mainHTML, LoginPage);
   });
-
-  router.on('/about-page', function () {
+  router.on('/about-page', () => {
     const AboutPage = new About().render();
     renderNewPage(mainHTML, AboutPage);
   });
+  router.navigate(`${window.location.hash.slice(2)}`);
 
-  router.navigate('/main-page');
+  window.addEventListener('hashchange', function () {
+    console.log('hash was changed');
+    const hash = window.location.hash.slice(2);
+    if (pagesList.includes(hash)) {
+      router.navigate(`/${hash}`);
+    } else {
+      mainHTML.clearInnerHTML();
+      console.log('404 - Page is not found');
+      router.on(`/$error-page`, function () {
+        console.log('error');
+      });
+    }
+  });
 }
-
-// TODO: add change page content when add new hash in URL
-
-// window.addEventListener('hashchange', function () {
-//   console.log('hash was changed');
-//   mainHTML.clearInnerHTML();
-//   const hash = window.location.hash;
-//   router.navigate(`${hash}`);
-// });
 
 // TODO: open error page when URL doesn't exist
