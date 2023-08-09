cb97bf63a29c7cf883c618451bb3323219289655
aniretakey <EMKornishina@gmail.com>
2023-08-09 22:27:25 +0300

    refactor: change class BaseComponent

--- src/utils/baseComponent.ts
+++ src/utils/baseComponent.ts
@@ -1,70 +1,75 @@
-export default class BaseComponent {
-  #node: HTMLElement;
+export default class BaseComponent<K extends keyof HTMLElementTagNameMap> {
+  protected node: HTMLElementTagNameMap[K];
 
   constructor({
-    tagName = 'div',
+    tagName,
     classNames = [],
     textContent = '',
     parentNode,
     attributes = {},
   }: {
-    tagName: string;
+    tagName: K;
     classNames?: string[];
     textContent?: string;
     parentNode?: HTMLElement;
     attributes?: Record<string, string>;
   }) {
-    this.#node = document.createElement(tagName);
+    this.node = document.createElement(tagName);
     if (classNames) {
-      this.#node.classList.add(...classNames);
+      this.node.classList.add(...classNames);
     }
     if (textContent) {
-      this.#node.textContent = textContent;
+      this.node.textContent = textContent;
     }
     if (attributes) {
       this.setAttributes(attributes);
     }
     if (parentNode) {
-      parentNode.append(this.#node);
+      parentNode.append(this.node);
     }
   }
 
-  public append(child: BaseComponent): void {
-    this.#node.append(child.getNode());
+  public append(child: BaseComponent<keyof HTMLElementTagNameMap>): void {
+    this.node.append(child.getNode());
   }
 
-  public appendChildren(children: BaseComponent[]): void {
+  public appendChildren(children: BaseComponent<K>[]): void {
     children.forEach((el) => {
       this.append(el);
     });
   }
 
-  public getNode(): HTMLElement {
-    return this.#node;
+  public getNode(): HTMLElementTagNameMap[K] {
+    return this.node;
   }
 
   public clearInnerHTML(): void {
-    this.#node.innerHTML = '';
+    this.node.innerHTML = '';
   }
 
   public addClass(classNames: string[]): void {
     classNames.forEach((className) => {
-      this.#node.classList.add(className);
+      this.node.classList.add(className);
     });
   }
 
   public setAttributes(attributes: Record<string, string>): void {
-    Object.entries(attributes).forEach(([prop, value]) => this.#node.setAttribute(prop, value));
+    Object.entries(attributes).forEach(([prop, value]) => this.node.setAttribute(prop, value));
   }
 
   public addListener<T extends keyof HTMLElementEventMap>(
     eventName: T,
-    callback: (event: HTMLElementEventMap[T]) => void,
-  ): void {
-    this.#node.addEventListener(eventName, callback);
+    callback: (event: Event) => void,
+  ): BaseComponent<K> {
+    this.node.addEventListener(eventName, callback);
+    return this;
+  }
+
+  public removeListener<T extends keyof HTMLElementEventMap>(eventName: T, callback: (event: Event) => void): void {
+    this.node.removeEventListener(eventName, callback);
   }
 
   public destroy(): void {
-    this.#node.remove();
+    this.node.remove();
   }
 }
