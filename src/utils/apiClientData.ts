38c41a64a9cd605a8f0749203c75604e444c9881
mracoon <nataliyakomarova777@gmail.com>
2023-08-11 22:56:49 +0300

    feat: add api client data

---
+++ src/utils/apiClientData.ts
@@ -0,0 +1,6 @@
+export const projectKey = 'comforto';
+export const clientID = 'n4ligPd7RBEI1LP2Ipio_net';
+export const clientSecret = 'Sa6hXHtDm2i9wqtN52pR-CbYMVmQC4FH';
+export const scope =
+  'manage_my_quotes:comforto manage_my_profile:comforto create_anonymous_token:comforto view_categories:comforto manage_my_shopping_lists:comforto manage_my_quote_requests:comforto manage_my_business_units:comforto manage_my_orders:comforto view_products:comforto view_published_products:comforto manage_my_payments:comforto';
+export const region = 'europe-west1.gcp';
