582172883f966e5844a3bd41dce5133221d8ee32
aniretakey <EMKornishina@gmail.com>
2023-08-11 21:59:01 +0300

    feat: update enums for form

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -1,4 +1,9 @@
 export enum FormFields {
   email = 'Email',
   password = 'Password',
+  firstName = 'First name',
+  lastName = 'Last name',
+  street = 'Street',
+  city = 'City',
+  postalCode = 'Postal code',
 }
