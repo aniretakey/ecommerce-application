196a7f8fc9f33954bc063a231b93602b3cb69cb1
mracoon <nataliyakomarova777@gmail.com>
2023-08-11 11:39:35 +0300

    feat: add redirect btn to form

--- src/components/form/FormTemplate.ts
+++ src/components/form/FormTemplate.ts
@@ -1,4 +1,4 @@
-import { FormFields } from '@customTypes/enums';
+import { FormFields, FormPages, FormRedirectBtn, FormSubmitBtn, RedirectMessage } from '@customTypes/enums';
 import BaseComponent from '@utils/baseComponent';
 import { safeQuerySelector } from '@utils/safeQuerySelector';
 import { ValidationCb } from '@customTypes/types';
@@ -9,9 +9,10 @@ export class Form {
   public form: BaseComponent<'form'>;
   public submitBtn: BaseComponent<'button'>;
   protected formFields: HTMLDivElement[] = [];
-  private pageName: string;
+  private pageName: FormPages;
+  protected redirectBtn: BaseComponent<'button'>;
 
-  constructor(pageName: string) {
+  constructor(pageName: FormPages) {
     this.pageName = pageName;
     this.form = new BaseComponent({
       tagName: 'form',
@@ -22,17 +23,38 @@ export class Form {
     this.submitBtn = new BaseComponent({
       tagName: 'button',
       classNames: [`${pageName}__submit-btn`],
-      textContent: 'Log In',
-      attributes: { type: 'submit', id: `${pageName}SubmitBtn` },
+      textContent: FormSubmitBtn[pageName],
+      attributes: { id: `${pageName}SubmitBtn` },
     });
+
+    this.redirectBtn = new BaseComponent({
+      tagName: 'button',
+      classNames: [`${pageName}__redirect-btn`],
+      textContent: FormRedirectBtn[pageName],
+      attributes: { id: `${pageName}RedirectBtn` },
+    }).addListener('click', this.formRedirect.bind(this));
+  }
+
+  private formRedirect(): void {
+    // TODO add redirection to login/registration page
   }
 
   public addSubmitListener(submitHandler: (event: Event) => void): this {
-    this.submitBtn.addListener('submit', submitHandler);
+    this.submitBtn.addListener('click', submitHandler);
     return this;
   }
 
-  //create field with validation like email or password
+  /**
+   *create field with validation like email or password
+   *
+   * ```html
+   * <div class="login__email-container form-field-container" data-valid="">
+   *    <label class="login__email-label" for="loginEmail">Email*</label>
+   *    <input class="login__email-input" type="text" id="loginEmail">
+   *    <p class="error-message" id="loginEmailError"></p>
+   * </div>
+   * ```
+   */
   protected addNewValidatedField(
     fieldName: FormFields,
     inputType = 'text',
@@ -50,7 +72,18 @@ export class Form {
     return this;
   }
 
-  // create btn like show password btn
+  /**
+   * create btn like show password btn
+   * without a field for displaying an error, but you can add a handler to the input through eventHandler
+   *
+   * ```html
+   * <div class="login__show-pw-container form-field-container" data-valid="">
+   *  <label class="login__show-pw-label" for="loginshow-pw">Show Password</label>
+   *  <input class="login__show-pw-input" type="checkbox" id="loginshow-pw">
+   * </div>
+   *
+   * ```
+   */
   protected addNewCtrlField(
     fieldName: FormFields,
     inputType = 'checkbox',
@@ -71,7 +104,22 @@ export class Form {
   }
 
   protected buildForm(): this {
-    this.form.getNode().append(...this.formFields, this.submitBtn.getNode());
+    this.form
+      .getNode()
+      .append(
+        ...this.formFields,
+        this.submitBtn.getNode(),
+        this.createRedirectMessage().getNode(),
+        this.redirectBtn.getNode(),
+      );
     return this;
+  }
+  private createRedirectMessage(): BaseComponent<'p'> {
+    const p = new BaseComponent({
+      tagName: 'p',
+      classNames: ['redirect-message'],
+      textContent: RedirectMessage[this.pageName],
+    });
+    return p;
   }
 }
