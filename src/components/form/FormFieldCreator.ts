7c5534cdc9bc85efbd21cb50319f79e8ee6beff9
mracoon <98079971+mracoon@users.noreply.github.com>
2023-08-11 21:55:38 +0300

    feat: add login page with validation
    * feat: add zod
    
    * feat: add email & password validation
    
    * feat: add tooltip for validator
    
    * feat: add favicon
    
    * chore: update path aliases
    
    * feat: add pages, utils, types
    
    * feat: pull routing
    
    * feat: add login page layout
    
    * feat: add styles to login page
    
    * feat: add hide/show password
    
    * feat: return more specific element from BaseComponent
    
    * refactor: delete files, upd validation
    
    * feat: add form field creator
    
    * feat: add login form
    
    * feat: add enums for form
    
    * feat: add redirect btn to form
    
    * refactor: upd form docs
    
    * refactor: refactor form
    
    * feat: add form styles
    
    * chore: remove router alias
    
    * feat: add navigation to registration page

---
+++ src/components/form/FormFieldCreator.ts
@@ -0,0 +1,97 @@
+import { FormFields, FormPages } from '@customTypes/enums';
+import { ValidationCb } from '@customTypes/types';
+import BaseComponent from '@utils/baseComponent';
+import { validator } from '@utils/validator';
+import { ZodString } from 'zod';
+
+const labelClassnames = ['block', 'text-gray-700', 'text-sm', 'font-bold', 'mb-2'];
+const inputclassNames = [
+  'shadow',
+  'appearance-none',
+  'border',
+  'rounded',
+  'w-full',
+  'py-2',
+  'px-3',
+  'text-gray-700',
+  'leading-tight',
+  'focus:outline-none',
+  'focus:shadow-outline',
+];
+
+/**
+ * ```html
+ * <div class="login__email-container form-field-container" data-valid="">
+ *    <label class="login__email-label" for="loginEmail">Email*</label>
+ *    <input class="login__email-input" type="text" id="loginEmail">
+ *    <p class="error-message" id="loginEmailError"></p>
+ * </div>
+ * ```
+ */
+export class FormFieldCreator {
+  public fieldContainer: BaseComponent<'div'>;
+  public fieldLabel: BaseComponent<'label'>;
+  public fieldInput: BaseComponent<'input'>;
+  public fieldError: BaseComponent<'p'> | null = null;
+
+  constructor(
+    page: FormPages,
+    fieldName: FormFields,
+    inputType: string,
+    isErrMessRequired = true,
+    labelText = `${fieldName}*`,
+  ) {
+    this.fieldContainer = new BaseComponent({
+      tagName: 'div',
+      classNames: [`${page}__${fieldName}-container`.toLowerCase(), 'form-field-container'],
+      attributes: { 'data-valid': '' },
+    });
+    this.fieldLabel = new BaseComponent({
+      tagName: 'label',
+      classNames: [`${page}__${fieldName}-label`.toLowerCase(), ...labelClassnames],
+      textContent: `${labelText}`,
+      attributes: { for: `${page}${fieldName}` },
+      parentNode: this.fieldContainer.getNode(),
+    });
+    this.fieldInput = new BaseComponent({
+      tagName: 'input',
+      classNames: [`${page}__${fieldName}-input`.toLowerCase(), ...inputclassNames],
+      attributes: { type: inputType, id: `${page}${fieldName}` },
+      parentNode: this.fieldContainer.getNode(),
+    });
+    if (isErrMessRequired) {
+      this.fieldError = new BaseComponent({
+        tagName: 'p',
+        classNames: ['error-message', 'text-red-500', 'text-xs', 'italic'],
+        textContent: '', //'Invalid email',
+        attributes: { id: `${page}${fieldName}Error` },
+        parentNode: this.fieldContainer.getNode(),
+      });
+    }
+  }
+
+  public addInputValidation(
+    validatorField: ZodString, // validator.email or validator.password
+    validationCb?: ValidationCb,
+  ): this {
+    this.fieldInput.addListener('input', this.addErrMessage.bind(this, validatorField, validationCb));
+    return this;
+  }
+
+  private addErrMessage(
+    validatorField: ZodString, // validator.email or validator.password
+    validationCb?: ValidationCb,
+  ): this {
+    if (this.fieldError) {
+      const errorMessage = validator.validate(validatorField, this.fieldInput.getNode().value, validationCb);
+      if (errorMessage) {
+        this.fieldError.getNode().textContent = errorMessage;
+        this.fieldContainer.setAttributes({ 'data-valid': '' });
+      } else {
+        this.fieldError.getNode().textContent = '';
+        this.fieldContainer.setAttributes({ 'data-valid': 'true' });
+      }
+    }
+    return this;
+  }
+}
--- src/types/enums.ts
+++ src/types/enums.ts
@@ -1,4 +1,25 @@
 export enum FormFields {
   email = 'Email',
   password = 'Password',
+  showPw = 'show-pw',
+}
+
+export enum FormPages {
+  login = 'login',
+  registration = 'registration',
+}
+
+export enum RedirectMessage {
+  login = 'Have not account yet?',
+  registration = 'Already have an account?',
+}
+
+export enum FormSubmitBtn {
+  login = 'Sign In',
+  registration = 'Sign Up',
+}
+
+export enum FormRedirectBtn {
+  login = 'Sign Up',
+  registration = 'Sign In',
 }
