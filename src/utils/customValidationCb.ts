6585e389e4f1a3571e14107e0fdfbf70c0b4263b
aniretakey <EMKornishina@gmail.com>
2023-08-12 18:04:08 +0300

    feat: add validation rules for registration

--- src/utils/customValidationCb.ts
+++ src/utils/customValidationCb.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod';
 import { FormFields } from '@customTypes/enums';
 import { ValidationCb } from '@customTypes/types';
-import { checkMandatory, checkMatch, checkMinLength, checkWhitespaces } from './validationHelper';
+import { checkMandatory, checkMatch, checkMinLength, checkMaxLength, checkWhitespaces } from './validationHelper';
 
 const MIN_PASSWORD_LENGTH = 8;
+const MIN_LENGTH = 1;
+const POSTAL_CODE_LENGTH = 6;
 
 export const passwordValidationCb: ValidationCb = (val: string, ctx: z.RefinementCtx) => {
   checkMandatory(val, ctx);
@@ -52,6 +54,58 @@ export const emailValidationCb: ValidationCb = (val: string, ctx: z.RefinementCt
     ctx.addIssue({
       code: z.ZodIssueCode.custom,
       message: errMessage,
+    });
+  }
+};
+
+export const firstNameValidationCb: ValidationCb = (val: string, ctx: z.RefinementCtx) => {
+  checkMandatory(val, ctx);
+  checkMinLength(val, ctx, MIN_LENGTH, FormFields.firstName);
+  checkMatch(val, ctx, /(^[а-яa-zА-ЯA-ZЁё]+$)/, 'First name must contain only letters');
+};
+
+export const lastNameValidationCb: ValidationCb = (val: string, ctx: z.RefinementCtx) => {
+  checkMandatory(val, ctx);
+  checkMinLength(val, ctx, MIN_LENGTH, FormFields.lastName);
+  checkMatch(val, ctx, /(^[а-яa-zА-ЯA-ZЁё]+$)/, 'Last name must contain only letters');
+};
+
+export const countryValidationCb: ValidationCb = (val: string, ctx: z.RefinementCtx) => {
+  checkMandatory(val, ctx);
+  checkMinLength(val, ctx, MIN_LENGTH, FormFields.country);
+  checkMatch(val, ctx, /^(Russia|Россия)$/i, 'Sorry, we only deliver to Russia');
+};
+
+export const cityValidationCb: ValidationCb = (val: string, ctx: z.RefinementCtx) => {
+  checkMandatory(val, ctx);
+  checkMinLength(val, ctx, MIN_LENGTH, FormFields.city);
+  checkMatch(val, ctx, /(^[а-яa-zА-ЯA-ZЁё]+$)/, 'City name must contain only letters');
+};
+
+export const streetValidationCb: ValidationCb = (val: string, ctx: z.RefinementCtx) => {
+  checkMandatory(val, ctx);
+  checkMinLength(val, ctx, MIN_LENGTH, FormFields.street);
+  checkMatch(val, ctx, /^[а-яa-zА-ЯA-Z0-9 ]*$/, 'Street name must contain only letters, numbers and spaces');
+};
+
+export const postalCodeValidationCb: ValidationCb = (val: string, ctx: z.RefinementCtx) => {
+  checkMatch(val, ctx, /^[0-9]*$/, 'Postal code must contain only numbers');
+  checkMandatory(val, ctx);
+  checkMinLength(val, ctx, 6, FormFields.postalCode);
+  checkMaxLength(val, ctx, POSTAL_CODE_LENGTH, FormFields.postalCode);
+};
+
+export const ageValidationCb: ValidationCb = (val: string, ctx: z.RefinementCtx) => {
+  const YEAR_IN_MSEC = 31536000000;
+  const nowDate = new Date().getTime();
+  const birthDate = new Date(val).getTime();
+  const diffInMSEC = nowDate - birthDate;
+  const diffInYears = Math.floor(diffInMSEC / YEAR_IN_MSEC);
+
+  if (diffInYears < 14) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: `You must be at least 14 years old`,
     });
   }
 };
