6138cbdb6fb4179d608d1d496a9ddefa239e14d4
mracoon <nataliyakomarova777@gmail.com>
2023-08-18 20:18:07 +0300

    feat: add checkMinLength & checkMaxLength tests

---
+++ src/tests/checkLength.test.ts
@@ -0,0 +1,51 @@
+import { z } from 'zod';
+import { checkMaxLength, checkMinLength } from '../utils/validationHelper';
+import { FormFields } from '../types/enums';
+import { addIssueMock, createTestLengthData, ctxMock } from './testHelper';
+
+const minLength = 5;
+const fieldName = FormFields.firstName;
+const maxLength = 10;
+const minMessage = `${fieldName} must be at least ${minLength} characters long`;
+const maxMessage = `${fieldName} must have no more than ${maxLength} characters long`;
+
+describe.each(createTestLengthData(minLength, maxLength))(`checkMinLength & checkMaxLength tests`, (value) => {
+  describe(`${value.length} character(s): ${value}`, () => {
+    beforeEach(() => {
+      addIssueMock.mockClear();
+    });
+    const len = value.length;
+    if (len < minLength) {
+      it(`checkMinLength should call the addIssue method if the length is less than ${minLength} characters `, () => {
+        checkMinLength(value, ctxMock, minLength, fieldName);
+        expect(ctxMock.addIssue).toHaveBeenCalledWith({
+          code: z.ZodIssueCode.custom,
+          message: minMessage,
+        });
+      });
+    } else {
+      it(`checkMinLength should not call the addIssue method if the length is greater than ${minLength} characters `, () => {
+        checkMinLength(value, ctxMock, minLength, fieldName);
+        expect(ctxMock.addIssue).not.toHaveBeenCalled();
+      });
+    }
+    if (len > maxLength) {
+      it(`checkMaxLength should call the addIssue method if the length is greater than ${maxLength} characters `, () => {
+        checkMaxLength(value, ctxMock, maxLength, FormFields.firstName);
+        expect(ctxMock.addIssue).toHaveBeenCalledWith({
+          code: z.ZodIssueCode.custom,
+          message: maxMessage,
+        });
+      });
+    } else {
+      it(`checkMaxLength should not call the addIssue method if the length is less than ${maxLength} characters `, () => {
+        checkMaxLength(value, ctxMock, maxLength, FormFields.firstName);
+        expect(ctxMock.addIssue).not.toHaveBeenCalled();
+      });
+    }
+  });
+});
+
+afterAll(() => {
+  addIssueMock.mockClear();
+});
--- src/tests/checkMandatory.test.ts
+++ src/tests/checkMandatory.test.ts
@@ -1,12 +1,8 @@
 import { z } from 'zod';
-import { checkMandatory } from '../utils/validationHelper'; // Подставьте путь к модулю с вашей функцией
+import { checkMandatory } from '../utils/validationHelper';
+import { addIssueMock, ctxMock } from './testHelper';
 
-const addIssueMock = jest.fn();
-
-const ctxMock: z.RefinementCtx = {
-  addIssue: addIssueMock,
-  path: [],
-};
+const message = 'This field is mandatory';
 
 describe('checkMandatory tests', () => {
   describe('field is empty', () => {
@@ -14,36 +10,34 @@ describe('checkMandatory tests', () => {
       const value = '';
       checkMandatory(value, ctxMock);
     });
-    const message = 'This field is mandatory';
     it('should call addIssue method if the field is empty', () => {
       expect(ctxMock.addIssue).toBeCalled();
     });
-
     it(`should add new issue with message '${message}' if the field is empty`, () => {
       expect(ctxMock.addIssue).toHaveBeenCalledWith({
         code: z.ZodIssueCode.custom,
         message,
       });
     });
-
     afterAll(() => {
       addIssueMock.mockClear(); //clears information about past calls after each test
     });
   });
+
   describe('field is not empty', () => {
     describe.each([
       [' ', 'one space'],
-      ['!', 'one special character'],
-      ['a', 'one letter'],
-      ['1', 'one digit'],
+      ['a', 'one character'],
       ['abc123', 'multiple characters'],
     ])(`should not call addIssue method if there is at least one character in the field`, (val, mess) => {
-      // const value = val
       checkMandatory(val, ctxMock);
 
       it(`${mess}: '${val}'`, () => {
         expect(ctxMock.addIssue).not.toHaveBeenCalled();
       });
     });
+  });
+  afterAll(() => {
+    addIssueMock.mockClear(); //clears information about past calls after each test
   });
 });
