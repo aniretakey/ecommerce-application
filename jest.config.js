a542844fc304cd4a6003b2dc915e7b04c009d508
mracoon <nataliyakomarova777@gmail.com>
2023-08-19 22:30:04 +0300

    chore: add aliases

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,11 @@ module.exports = {
   verbose: true,
   moduleNameMapper: {
     '^@customTypes/(.*)$': '<rootDir>/src/types/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
   },
   collectCoverageFrom: ['src/*/*/*', 'src/*/*', 'src/*'],
-  coveragePathIgnorePatterns: ['/node_modules/', 'src/tests/*', 'src/types/*'],
+  coveragePathIgnorePatterns: ['/node_modules/', 'src/tests/*', 'src/types/*', 'src/utils/apiClientData.ts'],
 };
