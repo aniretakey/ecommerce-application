cea96f021518f22c6ee82656a2a87852527d8dc3
mracoon <nataliyakomarova777@gmail.com>
2023-08-19 10:31:22 +0300

    chore: config test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  verbose: true,
   moduleNameMapper: {
     '^@customTypes/(.*)$': '<rootDir>/src/types/$1',
   },
+  collectCoverageFrom: ['src/*/*/*', 'src/*/*', 'src/*'],
+  coveragePathIgnorePatterns: ['/node_modules/', 'src/tests/*', 'src/types/*'],
 };
