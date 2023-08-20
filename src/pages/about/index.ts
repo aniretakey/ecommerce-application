a54ea39b68dd77e640873980c3b97369a20115a9
aniretakey <EMKornishina@gmail.com>
2023-08-20 11:34:13 +0300

    fix: moved the user info button to the profile

--- src/pages/about/index.ts
+++ src/pages/about/index.ts
@@ -1,5 +1,3 @@
-import { getCustomer } from '@utils/apiRequests';
-import BaseComponent from '@utils/baseComponent';
 import Page from '@utils/pageTemplate';
 
 export default class About extends Page {
@@ -9,31 +7,6 @@ export default class About extends Page {
 
   public render(): HTMLElement {
     const title = this.createHeaderTitle('About');
-
-    const greeting = new BaseComponent({
-      tagName: 'h2',
-      classNames: ['text-gray-700', 'font-bold', 'mb-2'],
-      textContent: '',
-    });
-
-    const aboutMeBtn = new BaseComponent({
-      tagName: 'button',
-      classNames: ['btn', 'btn-primary'],
-      textContent: 'Customer Info',
-    });
-    aboutMeBtn.getNode().addEventListener('click', () => {
-      getCustomer()
-        .then((data) => {
-          console.log(data);
-          greeting.getNode().textContent = `My name is  ${data.body.firstName}`;
-        })
-        .catch((err) => {
-          console.log(err);
-          greeting.getNode().textContent = `Please, sign In`;
-        });
-    });
-
-    this.container.append(title, aboutMeBtn.getNode(), greeting.getNode());
-    return this.container;
+    return title;
   }
 }
