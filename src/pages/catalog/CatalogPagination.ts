afa5b56e26ce7d5ca86e32c22b723b3317bc5095
mracoon <nataliyakomarova777@gmail.com>
2023-08-23 23:01:55 +0300

    feat: add pagination

---
+++ src/pages/catalog/CatalogPagination.ts
@@ -0,0 +1,19 @@
+import BaseComponent from '@utils/baseComponent';
+
+export class CatalogPagination {
+  public pagContainer: BaseComponent<'div'>;
+  public prevBtn: BaseComponent<'button'>;
+  public pageInfoBtn: BaseComponent<'button'>;
+  public nextBtn: BaseComponent<'button'>;
+  constructor() {
+    this.pagContainer = new BaseComponent({ tagName: 'div', classNames: ['join'] });
+    this.prevBtn = new BaseComponent({ tagName: 'button', classNames: ['join-item', 'btn'], textContent: '«' });
+    this.pageInfoBtn = new BaseComponent({
+      tagName: 'button',
+      classNames: ['join-item', 'btn'],
+      textContent: 'Page 1',
+    });
+    this.nextBtn = new BaseComponent({ tagName: 'button', classNames: ['join-item', 'btn'], textContent: '»' });
+    this.pagContainer.appendChildren([this.prevBtn, this.pageInfoBtn, this.nextBtn]);
+  }
+}
--- src/pages/catalog/CatalogView.ts
+++ src/pages/catalog/CatalogView.ts
@@ -4,6 +4,8 @@ import { getCategories, getProducts } from '@utils/apiRequests';
 import { Product } from '@commercetools/platform-sdk';
 import placeholder from '@assets/logo.png';
 import { CatalogCard } from '@pages/catalog/catalogCardTemplate';
+import { CatalogPagination } from './CatalogPagination';
+import { CATALOG_CARDS_NUM } from '@customTypes/enums';
 
 export class CatalogView {
   public catalogWrapper: BaseComponent<'div'>;
@@ -11,6 +13,10 @@ export class CatalogView {
   private catalogCardsWrap: BaseComponent<'div'>;
   private categoryData: Record<string, string> = {};
   private cardItems: CatalogCard[] = [];
+  private currentPage = 1;
+  //  private maxPage = 1;
+  private pagination: CatalogPagination;
+  private total = 6;
   constructor() {
     this.catalogWrapper = new BaseComponent({
       tagName: 'div',
@@ -19,14 +25,38 @@ export class CatalogView {
       tagName: 'div',
       classNames: ['catalog__cards-wrap', 'justify-items-center'],
     });
+    this.pagination = new CatalogPagination();
     this.filtersContainer = new CatalogFilters();
-    this.catalogWrapper.appendChildren([this.filtersContainer.filters, this.catalogCardsWrap]);
+    this.catalogWrapper.appendChildren([
+      this.filtersContainer.filters,
+      this.catalogCardsWrap,
+      this.pagination.pagContainer,
+    ]);
     this.draw();
     this.drawCatalogCards();
+
+    this.pagination.nextBtn.addListener('click', () => {
+      this.currentPage += 1;
+      this.cardItems = [];
+      this.draw();
+      this.drawCatalogCards((this.currentPage - 1) * CATALOG_CARDS_NUM, CATALOG_CARDS_NUM);
+
+      this.pagination.pageInfoBtn.setTextContent(`Page ${this.currentPage}`);
+    });
+    this.pagination.prevBtn.addListener('click', () => {
+      this.currentPage -= 1;
+      this.cardItems = [];
+      this.draw();
+      this.drawCatalogCards((this.currentPage - 1) * CATALOG_CARDS_NUM, CATALOG_CARDS_NUM);
+
+      this.pagination.pageInfoBtn.setTextContent(`Page ${this.currentPage}`);
+    });
   }
 
   private draw(): void {
-    for (let i = 0; i < 6; i++) {
+    this.catalogCardsWrap.clearInnerHTML();
+    console.log(this.total, this.currentPage);
+    for (let i = 0; i < Math.min(6, this.total - (this.currentPage - 1) * CATALOG_CARDS_NUM); i++) {
       const newItem = new CatalogCard().buildItem();
       this.catalogCardsWrap.getNode().append(newItem.card.getNode());
       this.cardItems.push(newItem);
@@ -47,11 +77,13 @@ export class CatalogView {
       .catch(console.log);
   }
 
-  private drawCatalogCards(): void {
-    getProducts()
+  private drawCatalogCards(offset = 0, limit = CATALOG_CARDS_NUM): void {
+    getProducts(offset, limit)
       .then(async (data) => {
+        this.total = data.body.total ?? 0;
+        //   this.maxPage = data.body.total ?? 1;
+        console.log(data);
         await this.getCategoryNames();
-        console.log(this.categoryData, this.categoryData.size);
         const results = data.body.results;
         this.createItems(results, this.categoryData);
       })
@@ -59,7 +91,7 @@ export class CatalogView {
   }
 
   private createItems(results: Product[], categoryNames: Record<string, string>): void {
-    if (results.length > 1) {
+    if (results.length >= 1) {
       results.forEach((item, index) => {
         const product = item.masterData.current;
         const categories: string[] = item.masterData.current.categories.map((data) => categoryNames[data.id] ?? '');
@@ -77,13 +109,18 @@ export class CatalogView {
         if (images.length) {
           imgSrc = images[0]?.url ?? `${placeholder}`;
         }
-        console.log(this.cardItems[index]);
+
         this.cardItems[index]
           ?.setProductName(name)
           .setProductDescription(description)
           .setPhotoAttr(imgSrc, name)
           .setCategories(categories)
           .displayPrice(price, discount);
+      });
+    } //удаление лишних карточек
+    if (results.length < this.cardItems.length) {
+      this.cardItems.slice(results.length).forEach((el) => {
+        el.card.destroy();
       });
     }
   }
