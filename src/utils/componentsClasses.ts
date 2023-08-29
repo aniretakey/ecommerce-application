8e8749bb2b7fafc9553478927482c8be7198f785
mracoon <nataliyakomarova777@gmail.com>
2023-08-29 16:59:43 +0300

    refactor: refactor search

--- src/utils/componentsClasses.ts
+++ src/utils/componentsClasses.ts
@@ -8,3 +8,33 @@ export const filterClasses = [
   'rounded-lg',
   'items-center',
 ];
+export const searchBtnClasses = [
+  'btn-primary',
+  'text-white',
+  'absolute',
+  'right-2.5',
+  'bottom-2.5',
+  'font-medium',
+  'rounded-lg',
+  'text-sm',
+  'px-4',
+  'py-2',
+];
+export const searchInputClasses = [
+  'block',
+  'w-full',
+  'p-4',
+  'pl-10',
+  'text-sm',
+  'text-gray-900',
+  'border',
+  'border-gray-300',
+  'rounded-lg',
+  'bg-gray-50',
+  'focus:outline-none',
+  'focus:border-primary',
+];
+export const searchIconSvg = `
+<svg class="absolute inset-4 pointer-events-none w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+</svg>`;
