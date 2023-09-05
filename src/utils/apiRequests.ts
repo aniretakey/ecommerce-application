2c5fa17a3cfa314e8f35f2025921df823f16da3f
gorobchenko-k <gorobchenko.es@mail.ru>
2023-09-05 19:49:05 +0300

    feat: add changeCard function

--- src/utils/apiRequests.ts
+++ src/utils/apiRequests.ts
@@ -226,3 +226,21 @@ export const addProductInCart = (ID: string, version: number, productId: string)
     })
     .execute();
 };
+
+export const changeCard = (changeAction: (data: ClientResponse<Cart>) => void, productId: string): void => {
+  const cartId = localStorage.getItem('comforto-cart-id');
+  if (cartId) {
+    getCart(cartId)
+      .then((data) => {
+        changeAction(data);
+      })
+      .catch(console.log);
+  } else {
+    createCart([{ productId: productId }])
+      .then((data) => {
+        const cartId = data.body.id;
+        localStorage.setItem('comforto-cart-id', cartId);
+      })
+      .catch(console.log);
+  }
+};
