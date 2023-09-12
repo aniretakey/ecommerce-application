9503601f6008930f0182e841cc1d28c15170e852
aniretakey <EMKornishina@gmail.com>
2023-09-12 22:10:07 +0300

    feat: add about us page

--- src/pages/about/index.ts
+++ src/pages/about/index.ts
@@ -1,12 +1,48 @@
 import Page from '@utils/pageTemplate';
+import BaseComponent from '@utils/baseComponent';
+import { teamMembers, collaborationInfo } from './teamMembersDescription';
+import createTeamMemberCard from './teamMemberCard';
+import './style.css';
 
 export default class About extends Page {
   constructor() {
     super('about');
   }
 
+  private collaborationInfoContainer = new BaseComponent({
+    tagName: 'div',
+    classNames: ['collaboration-info'],
+    textContent: collaborationInfo,
+  });
+
+  private cardsContainer = new BaseComponent({
+    tagName: 'div',
+    classNames: ['cards-container'],
+  });
+
+  private createTeamMemberCards(): BaseComponent<'div'> {
+    teamMembers.forEach((elem) => {
+      const card = createTeamMemberCard(
+        elem.name,
+        elem.role,
+        elem.gitHubLink,
+        elem.bio,
+        elem.photo,
+        elem.contributions,
+      );
+      this.cardsContainer.append(card);
+    });
+    return this.cardsContainer;
+  }
+
   public render(): HTMLElement {
-    const title = this.createHeaderTitle('About');
-    return title;
+    this.container.append(this.createHeaderTitle('About us'));
+    this.container.append(this.collaborationInfoContainer.getNode());
+    // const title = this.createHeaderTitle('About');
+    // return title;
+    // this.container.append(this.cardsElements);
+    this.createTeamMemberCards();
+    this.container.append(this.cardsContainer.getNode());
+    return this.container;
   }
 }
---
+++ src/pages/about/teamMembersDescription.ts
@@ -0,0 +1,51 @@
+export const teamMembers = [
+  {
+    name: 'Natalia Oreshkova',
+    role: 'Frontend Developer',
+    gitHubLink: 'https://github.com/mracoon',
+    bio: 'Studying frontend development since June 2022',
+    photo: '',
+    contributions: [
+      'Added full and comphensive README to the project',
+      'Set up CommerceTools Project and API Client',
+      'Implemented cart functionality',
+      'Added tests with Jest for the project',
+      'Set up inputs validation',
+      'Developed login, catalog pages',
+      'Set up products filtering, sorting and searching',
+    ],
+  },
+  {
+    name: 'Ekaterina Gorobchenko',
+    role: 'Frontend Developer',
+    gitHubLink: 'https://github.com/gorobchenko-k',
+    bio: 'I am studying frontend development since October 2022, I have a technical background and was partly related to frontend development',
+    photo: '',
+    contributions: [
+      'Created Development Environment Configuration',
+      'Implemented cart functionality',
+      'Set up categories navigation',
+      'Developed registration, login, user profile pages and header',
+      'Set up router (Navigo library)',
+      'Implemented modal window with images of products',
+    ],
+  },
+  {
+    name: 'Ekaterina Kornishina',
+    role: 'Frontend Developer',
+    gitHubLink: 'https://github.com/aniretakey',
+    bio: 'I graduated with a degree in Sociology and worked as a media analyst. Studying frontend development since December 2022',
+    photo: '',
+    contributions: [
+      'Set up Kanban task board',
+      'Added development scripts and set up repository',
+      'Set up Netlify for website deployment',
+      'Developed registration, products, main, about us and error pages',
+      'Implemented interactive product cards',
+      'Set up router (Navigo library)',
+    ],
+  },
+];
+
+export const collaborationInfo =
+  'Our effective collaboration for website development included clear communication, defined roles and responsibilities, discussions about progress and any issues, using project management tools and tasks distribution, setting clear goals and deadlines. We believe that the most important thing in successful teamwork is the ability to talk to each other';
