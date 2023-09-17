dc0fd49d0821ff045dcd6233adda51f546764633
Ekaterina Kornishina <121668605+aniretakey@users.noreply.github.com>
2023-09-17 10:48:05 +0300

    Feat/add about us page
    * feat: add about us page
    
    * feat: update about us page
    
    * feat: add RSS logo & info
    
    * feat: update style

---
+++ src/pages/about/aboutRSS.ts
@@ -0,0 +1,11 @@
+export const aboutRSS = `<footer class="footer p-10 text-base-content">
+  <aside>
+   <a href="https://rs.school/js/" target="_blank">
+    <img class="rss-logo" /> </a>
+  </aside>
+  <div class="footer-content">
+    <header class="footer-title">About Rolling Scopes School</header>
+    <p></p>
+    <p>RS School is free-of-charge and community-based education program conducted by The Rolling Scopes developer community since 2013. <br> Everyone can study at RS School, regardless of age, professional employment, or place of residence. <br> The mentors and trainers of our school are front-end and javascript developers from different companies and countries.</p>
+  </div>
+  </footer>`;
--- src/pages/about/index.ts
+++ src/pages/about/index.ts
@@ -1,12 +1,56 @@
 import Page from '@utils/pageTemplate';
+import BaseComponent from '@utils/baseComponent';
+import { teamMembers, collaborationInfo } from './teamMembersDescription';
+import createTeamMemberCard from './teamMemberCard';
+import { aboutRSS } from './aboutRSS';
+import './style.css';
 
 export default class About extends Page {
   constructor() {
     super('about');
   }
 
+  private collaborationInfoContainer = new BaseComponent({
+    tagName: 'div',
+    classNames: ['collaboration-info'],
+    textContent: collaborationInfo,
+  });
+
+  private cardsContainer = new BaseComponent({
+    tagName: 'div',
+    classNames: ['cards-container'],
+  });
+
+  private createAboutRSSContainer(): BaseComponent<'div'> {
+    const aboutRSSContainer = new BaseComponent({
+      tagName: 'div',
+      classNames: ['about-rss'],
+    });
+    aboutRSSContainer.getNode().innerHTML = aboutRSS;
+    return aboutRSSContainer;
+  }
+
+  private createTeamMemberCards(): BaseComponent<'div'> {
+    teamMembers.forEach((elem) => {
+      const card = createTeamMemberCard(
+        elem.name,
+        elem.role,
+        elem.gitHubLink,
+        elem.bio,
+        elem.photo,
+        elem.contributions,
+      );
+      this.cardsContainer.append(card);
+    });
+    return this.cardsContainer;
+  }
+
   public render(): HTMLElement {
-    const title = this.createHeaderTitle('About');
-    return title;
+    this.container.append(this.createHeaderTitle('About us'));
+    this.container.append(this.collaborationInfoContainer.getNode());
+    this.createTeamMemberCards();
+    this.container.append(this.cardsContainer.getNode());
+    this.container.append(this.createAboutRSSContainer().getNode());
+    return this.container;
   }
 }
